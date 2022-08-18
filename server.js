d1e0f00634f31b329e37157c8934e0d1e3b381ff
Mary Theofield <mptheofield@gmail.com>
2022-08-18 14:28:03 -0400

    adding functionality to notes to test in insomnia to check if it works

--- server.js
+++ server.js
@@ -1,9 +1,25 @@
+const fs = require('fs');
 const express = require("express");
+const path = require('path')
+const {notes} = require('./db/db.json')
+console.log({notes})
 
-const app = express();
 const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get('/api/notes', (req, res) => {
+  let results = notes;
+  res.json(results);
+});
 
 
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/notes.html'));
+});
 
 app.listen(PORT, () => {
     console.log(`API server on port http://localhost:${PORT}`);
