bd229c74edefcadb4421133f965d7a4995e65728
Mary Theofield <mptheofield@gmail.com>
2022-08-18 17:39:53 -0400

    following steps to add uuidc4 to project

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const path = require('path')
 const {notes} = require('./db/db.json')
 console.log({notes})
-const { v4: uuidv4 } = require("uuid");
+const { uuid } = require('uuidv4');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
