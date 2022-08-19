9eb1cc3eda6282cc5b2c3c8475acc12bf31a92c7
Mary Theofield <mptheofield@gmail.com>
2022-08-19 10:33:29 -0400

    organized files using the rooutte folders and ensured it is working using npm start

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,16 +1,37 @@
-const path = require("path");
+const fs = require("fs");
+let data = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+const { v4: uuidv4 } = require('uuid');
 
-module.exports = function(app) {
-    
-    app.get("/", function(req, res) {
-        res.sendFile(path.join(__dirname, "/../public/index.html"));
+module.exports = (app) =>{
+
+    app.get("/api/notes", (req, res) => {
+       
+        res.json(data);
+
     });
-    
-    app.get("/notes", function(req, res) {
-        res.sendFile(path.join(__dirname, "/../public/notes.html"));
+
+    app.get("/api/notes/:id", (req, res) =>{
+
+        res.json(data[Number(req.params.id)]);
+
     });
 
-    
 
+    app.post("/api/notes", (req, res) =>{
+
+        let newNote = req.body;
+        let uniqueId = (data.length).toString();
+        console.log(uniqueId);
+        newNote.id = uuidv4();
+        data.push(newNote);
+        
+        fs.writeFileSync("./db/db.json", JSON.stringify(data), (err) =>{
+            if (err) throw (err);        
+        }); 
+
+        res.json(data);    
+
+    });
 
 }
+
--- server.js
+++ server.js
@@ -1,45 +1,17 @@
 const fs = require('fs');
 const express = require("express");
-const path = require('path')
-const {notes} = require('./db/db.json')
-console.log({notes})
-const { v4: uuidv4 } = require('uuid');
-
-const PORT = process.env.PORT || 3001;
 const app = express();
+const PORT = process.env.PORT || 3001;
 
-app.use(express.static('public'));
+
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static("public"));
 
-app.get("/api/notes", (req, res) =>
-  res.sendFile(path.join(__dirname, "./db/db.json"))
-);
+require("./routes/htmlRoutes")(app);
+require("./routes/apiRoutes")(app);
 
-app.get('/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, './public/notes.html'));
-});
-
-app.get("/api/notes/:id", (req, res) =>
-  res.sendFile(path.join(__dirname, "./db/db.json"))
-);
-
-app.post("/api/notes", (req, res) => {
-  const noteSaved = req.body;
-  console.log(noteSaved);
-  noteSaved.id = uuid();
-  const notes = JSON.parse(
-    fs.readFileSync(path.join(__dirname, "./db/db.json"))
-  );
-  console.log(notes);
-  notes.push(noteSaved);
-  console.log(notes);
-  fs.writeFile("./db/db.json", JSON.stringify(notes), function (err, result) {
-    if (err) console.log("error", err);
-  });
-  return res.json({});
-});
 
 app.listen(PORT, () => {
     console.log(`API server on port http://localhost:${PORT}`);
