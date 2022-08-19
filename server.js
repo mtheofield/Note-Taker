5a91a17627456a7f0d6439663e23ee03b0ddc53a
Mary Theofield <mptheofield@gmail.com>
2022-08-18 20:58:43 -0400

    usng app.post getting the data from the server and creating a uniique id

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const path = require('path')
 const {notes} = require('./db/db.json')
 console.log({notes})
-const { uuid } = require('uuidv4');
+const { v4: uuidv4 } = require('uuid');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -17,7 +17,6 @@ app.get("/api/notes", (req, res) =>
   res.sendFile(path.join(__dirname, "./db/db.json"))
 );
 
-
 app.get('/notes', (req, res) => {
   res.sendFile(path.join(__dirname, './public/notes.html'));
 });
@@ -25,6 +24,22 @@ app.get('/notes', (req, res) => {
 app.get("/api/notes/:id", (req, res) =>
   res.sendFile(path.join(__dirname, "./db/db.json"))
 );
+
+app.post("/api/notes", (req, res) => {
+  const noteSaved = req.body;
+  console.log(noteSaved);
+  noteSaved.id = uuid();
+  const notes = JSON.parse(
+    fs.readFileSync(path.join(__dirname, "./db/db.json"))
+  );
+  console.log(notes);
+  notes.push(noteSaved);
+  console.log(notes);
+  fs.writeFile("./db/db.json", JSON.stringify(notes), function (err, result) {
+    if (err) console.log("error", err);
+  });
+  return res.json({});
+});
 
 app.listen(PORT, () => {
     console.log(`API server on port http://localhost:${PORT}`);
